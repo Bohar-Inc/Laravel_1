58bc67faef34065096e844106b17160d9bec778b
Thadsha98 <96372107+Thadsha98@users.noreply.github.com>
2024-11-16 10:36:28 +0530

    View basics and passing data

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,19 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('listings',[
+        'heading'=>'Latest things',
+        'listings'=>[
+            [
+                'id'=>1,
+                'title'=>'Listing one',
+                'description'=>'Laravel is a web application framework with expressive, elegant syntax. A web framework provides a structure and starting point for creating your application, allowing you to focus on creating something amazing while we sweat the details.',
+            ],
+            [
+                'id'=>2,
+                'title'=>'Listing two',
+                'description'=>'Laravel is a web application framework with expressive, elegant syntax. A web framework provides a structure and starting point for creating your application, allowing you to focus on creating something amazing while we sweat the details.',
+            ],
+        ]
+    ]);
 });
