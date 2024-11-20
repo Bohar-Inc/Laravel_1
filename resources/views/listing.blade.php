3430c23e9a8ad792ce505f0051ea44eee952a8eb
Thadsha98 <96372107+Thadsha98@users.noreply.github.com>
2024-11-20 11:23:51 +0530

    Blade components

--- resources/views/listing.blade.php
+++ resources/views/listing.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <a href="/" class="inline-block text-black ml-4 mb-4"><i class="fa-solid fa-arrow-left"></i> Back</a>
     <div class="mx-4">
-        <div class="bg-gray-50 border border-gray-200 p-10 rounded">
+        <x-card>
             <div class="flex flex-col items-center justify-center text-center">
                 <img class="w-48 mr-6 mb-6" src="{{asset('images/lara.png')}}" alt=""/>
 
@@ -42,6 +42,5 @@
                     </div>
                 </div>
             </div>
-        </div>
-    </div>
+    </x-card>
 @endsection
