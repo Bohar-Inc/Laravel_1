05a9cc866a66e1262438180d1d6c84116e36f729
Thadsha98 <96372107+Thadsha98@users.noreply.github.com>
2024-11-20 14:45:59 +0530

    Update listing

--- resources/views/listings/show.blade.php
+++ resources/views/listings/show.blade.php
@@ -3,7 +3,7 @@
     <div class="mx-4">
         <x-card>
             <div class="flex flex-col items-center justify-center text-center">
-                <img class="w-48 mr-6 mb-6" src="{{asset('images/lara.png')}}" alt=""/>
+                <img class="w-48 mr-6 mb-6" src="{{$listing->logo ? asset('storage/' . $listing->logo) :asset('images/lara.png')}}" alt=""/>
 
                 <h3 class="text-2xl mb-2">{{$listing->title}}</h3>
                 <div class="text-xl font-bold mb-4">{{$listing->company}}</div>
@@ -31,6 +31,12 @@
                     </div>
                 </div>
             </div>
+        </x-card>
+
+        <x-card class="mt-4 p-2 flex space-x-6">
+            <a href="/listings/{{$listing->id}}/edit">
+                <i class="fa-solid fa-pencil"></i> Edit
+            </a>
         </x-card>
     </div>
 </x-layout>
