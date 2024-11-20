f3bb13802e4067d1224720cf4c5a14eb9dcbd2bd
Thadsha98 <96372107+Thadsha98@users.noreply.github.com>
2024-11-20 17:04:31 +0530

    update

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -75,7 +75,7 @@ class ListingController extends Controller
             $formFields['logo'] = $request->file('logo')->store('logos','public');
         }
 
-        $formFields['user_id'] = auth()->id;
+        $formFields['user_id'] = auth()->id();
 
         $listing->update($formFields);
 
