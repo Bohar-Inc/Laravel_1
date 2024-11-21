8527ddd4b6751264c4e8fb6334d0a2b9111883ea
Thadsha98 <96372107+Thadsha98@users.noreply.github.com>
2024-11-21 17:11:33 +0530

    WIP

--- routes/web.php
+++ routes/web.php
@@ -4,14 +4,15 @@ use App\Models\Listing;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('listings',[
-            'heading' => 'Latest things',
+    return view('listings.index',[
             'listings' => Listing::all()
     ]);
 });
 
 Route::get('/listings/{id}', function ($id) {
-    return view('listing',[
+    return view('listings.show',[
         'listing' => Listing::find($id)
         ]);
 } );
+
+
