2daeafc92065050f0651706b57d22aa82ec8af8c
Thadsha98 <96372107+Thadsha98@users.noreply.github.com>
2024-11-21 13:25:09 +0530

    view basics and passing data, blade templates, create model, create table and running migrations

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,17 @@
 <?php
 
+use App\Models\Listing;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('listings',[
+            'heading' => 'Latest things',
+            'listings' => Listing::all()
+    ]);
 });
+
+Route::get('/listings/{id}', function ($id) {
+    return view('listing',[
+        'listing' => Listing::find($id)
+        ]);
+} );
