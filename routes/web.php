999ee04935de7d3aaf5b1dc79c9c90684d2b79ac
Thadsha98 <96372107+Thadsha98@users.noreply.github.com>
2024-11-22 12:06:01 +0530

    blade components, component attributes

--- routes/web.php
+++ routes/web.php
@@ -5,13 +5,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('listings',[
-        'heading' => 'Latest Listings',
         'listings' => Listing::all()
     ]);
 });
 
 Route::get('/listings/{listing}', function (Listing $listing) {
     return view('listing',[
-        'listing' => $listing,
+        'listing' => $listing
     ]);
 });
