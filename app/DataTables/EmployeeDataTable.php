2bca97eca198b25d3bbce6770867ae7034db56b1
Thadsha98 <96372107+Thadsha98@users.noreply.github.com>
2024-11-30 09:04:12 +0530

    Data tables create with options

---
+++ app/DataTables/EmployeeDataTable.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\DataTables;
+
+use App\Models\Employee;
+use Illuminate\Database\Eloquent\Builder as QueryBuilder;
+use Yajra\DataTables\EloquentDataTable;
+use Yajra\DataTables\Html\Builder as HtmlBuilder;
+use Yajra\DataTables\Html\Button;
+use Yajra\DataTables\Html\Column;
+use Yajra\DataTables\Html\Editor\Editor;
+use Yajra\DataTables\Html\Editor\Fields;
+use Yajra\DataTables\Services\DataTable;
+
+class EmployeeDataTable extends DataTable
+{
+    /**
+     * Build the DataTable class.
+     *
+     * @param QueryBuilder $query Results from query() method.
+     */
+    public function dataTable(QueryBuilder $query): EloquentDataTable
+    {
+        return (new EloquentDataTable($query))
+            ->addColumn('action', 'employee.action')
+            ->setRowId('id');
+    }
+
+    /**
+     * Get the query source of dataTable.
+     */
+    public function query(Employee $model): QueryBuilder
+    {
+        return $model->newQuery();
+    }
+
+    /**
+     * Optional method if you want to use the html builder.
+     */
+    public function html(): HtmlBuilder
+    {
+        return $this->builder()
+                    ->setTableId('employee-table')
+                    ->columns($this->getColumns())
+                    ->minifiedAjax()
+                    //->dom('BfrtiP')
+                    ->dom('<"top row"<"col-md-4"l><"col-md-4 text-center"B><"col-md-4"f>>t<"bottom row"<"col-md-6"i><"col-md-6 d-flex justify-content-end"p>>')
+                    ->orderBy(1)
+                    ->selectStyleSingle()
+                    ->buttons([
+                        Button::make('add'),
+                        Button::make('excel'),
+                        Button::make('csv'),
+                        Button::make('pdf'),
+                        Button::make('print'),
+                        Button::make('reset'),
+                        Button::make('reload'),
+                    ]);
+    }
+
+    /**
+     * Get the dataTable columns definition.
+     */
+    public function getColumns(): array
+    {
+        return [
+            'id',
+            'name',
+            'email',
+            'phone',
+            'salary',
+            'department',
+        ];
+    }
+
+    /**
+     * Get the filename for export.
+     */
+    protected function filename(): string
+    {
+        return 'Employee_' . date('YmdHis');
+    }
+}
---
+++ resources/views/employee.blade.php
@@ -0,0 +1,35 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Employee</title>
+
+    <style>
+        {!! Vite::content('resources/sass/app.scss') !!}
+    </style>
+    <script>
+        {!! Vite::content('resources/js/app.js') !!}
+    </script>
+{{--    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">--}}
+{{--    <link rel="stylesheet" href="https://cdn.datatables.net/2.1.8/css/dataTables.bootstrap5.min.css">--}}
+{{--    <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>--}}
+{{--    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>--}}
+{{--    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>--}}
+{{--    <script src="https://cdn.datatables.net/2.1.8/js/dataTables.min.js"></script>--}}
+{{--    <script src="https://cdn.datatables.net/2.1.8/js/dataTables.bootstrap5.min.js"></script>--}}
+
+</head>
+<body>
+    <section style="padding-top:60px;">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    {!! $dataTable->table() !!}
+                </div>
+            </div>
+        </div>
+    </section>
+    {!! $dataTable->scripts() !!}
+</body>
+</html>
