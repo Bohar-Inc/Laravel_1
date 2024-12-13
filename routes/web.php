9546dfd82f10b5dc8e8e3ae5d3e0dd845d7119c2
Thadsha98 <96372107+Thadsha98@users.noreply.github.com>
2024-12-13 16:49:05 +0530

    Full page components and navigation

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
+use App\Livewire\Counter;
+use App\Livewire\Lazy;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/counter',Counter::class);
+Route::get('/bird',\App\Livewire\BirdForm::class);
+Route::get('/lazy',Lazy::class)->lazy();
