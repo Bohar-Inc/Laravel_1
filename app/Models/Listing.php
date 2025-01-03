a8e8e8f8665a79d38c197320037ae9156262c930
SenthVyra <88665212+SenthVyra@users.noreply.github.com>
2025-01-03 15:02:49 +0530

    Theme
    Adding Theme

--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -2,34 +2,16 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Listing
+class Listing extends Model
+
 {
-    public static function all()
-    {
-        return [
-            [
-                'id' => 1,
-                'title' => 'Listing One',
-                'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam minima et illo reprehenderit quas possimus voluptas repudiandae cum expedita, eveniet aliquid, quam illum quaerat consequatur! Expedita ab consectetur tenetur deleniti.'
-            ],
-            [
-                'id' => 2,
-                'title' => 'Listing Two',
-                'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam minima et illo reprehenderit quas possimus voluptas repudiandae cum expedita, eveniet aliquid, quam illum quaerat consequatur! Expedita ab consectetur tenetur deleniti.'
-            ]
-        ];
-    }
 
-    public static function find($id)
-    {
-        $listings = self::all();
+    use HasFactory;
 
-        foreach ($listings as $listing) {
-            if ($listing['id'] == $id) {
-                return $listing;
-            }
-        }
-    }
+    // Add this line to enable factories
+
+    protected $fillable = ['title', 'tags', 'company', 'location'];
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Listing;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,11 +14,25 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        Listing::create([
+            'title' => 'Laravel Senior Developer',
+            'tags' => 'laravel, javascript',
+            'company' => 'Acme Corp',
+            'location' => 'Boston, MA',
+            'email' => 'email@email.com',
+            'website' => 'https://www.acme.com',
+            'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam minima et illo reprehenderit quas possimus voluptas repudiandae cum expedita, eveniet aliquid, quam illum quaerat consequatur! Expedita ab consectetur tenetur deleniti.'
+        ]);
+        Listing::create([
+            'title' => 'Software Engineer',
+            'tags' => 'python, django, aws',
+            'company' => 'Tech Solutions',
+            'location' => 'New York, NY',
+            'email' => 'careers@techsolutions.com',
+            'website' => 'https://www.techsolutions.co',
+            'description' => 'We are seeking a skilled Software Engineer to join our growing team. You will be involved in the design, development, and deployment of innovative software solutions.',
         ]);
     }
 }
